ac1d62d47e3bd5ddfd7c89fe07ab07a1ce149952
MohamedAfrasDev <mohamedafrasai@gmail.com>
2025-11-19 00:27:00 +0530

    Updated

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,11 @@
 import { PrismaClient } from "@/generated/prisma/client";
 
 declare global {
-  // Avoid multiple instances in dev
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query", "info", "warn", "error"], // optional
-  });
+  global.prisma ??
+  new PrismaClient({ log: ["query", "info", "warn", "error"] });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
