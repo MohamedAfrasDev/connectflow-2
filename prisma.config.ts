d66fdbc3f447fd069aed02f3d7da463208114188
MohamedAfrasDev <mohamedafrasai@gmail.com>
2025-11-19 01:04:59 +0530

    Updated

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,10 @@
+// prisma.config.ts
 import { defineConfig, env } from "prisma/config";
 import * as dotenv from "dotenv";
+dotenv.config();
 
-dotenv.config(); // <-- load .env manually
 export default defineConfig({
-  schema: "prisma/schema.prisma", // must be relative to the root
+  schema: "prisma/schema.prisma",
   migrations: { path: "prisma/migrations" },
-  engine: "classic", // library engine is required for serverless
-  datasource: { url: env("DATABASE_URL") },
-  // Removed 'client' property as it is not a known property in the Prisma config type
-})
+  datasource: { url: env("DATABASE_URL") }, // your Neon connection string
+});
